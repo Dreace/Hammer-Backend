8c2623d225489db1e706e920bf5fb5d4b92f9841
Dreace <Dreace@Foxmail.com>
2020-06-26 14:25:38 +0800

    fix: remove database test table

--- models/notice.py
+++ models/notice.py
@@ -4,7 +4,7 @@ from models.sqlalchemy_db import db
 
 
 class Notice(db.Model):
-    __tablename__ = 'notice_test'
+    __tablename__ = 'notice'
     __bind_key__ = 'notice'
     id_ = db.Column('id', db.Integer, primary_key=True)
     title = db.Column('标题', db.VARCHAR(255))
